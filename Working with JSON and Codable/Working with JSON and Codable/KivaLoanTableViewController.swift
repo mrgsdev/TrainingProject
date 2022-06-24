bd297ecb94758925035f9ef89f9a138e0eaeb365
mrgsdev <mrgsdev@gmail.com>
2022-06-24 11:24:16 +0300

    final

--- Working with JSON and Codable/Working with JSON and Codable/KivaLoanTableViewController.swift
+++ Working with JSON and Codable/Working with JSON and Codable/KivaLoanTableViewController.swift
@@ -9,16 +9,107 @@ import UIKit
 
 class KivaLoanTableViewController: UITableViewController {
 
+    private let kivaLoanURL = "https://api.kivaws.org/v1/loans/newest.json"
+    private var loans = [Loan]() 
+    
+    lazy var dataSource = configureDataSource()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.estimatedRowHeight = 92.0
         tableView.rowHeight = UITableView.automaticDimension
+        getLatestLoans()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
 
+extension KivaLoanTableViewController{
+    func getLatestLoans() {
+        guard let loanUrl = URL(string: kivaLoanURL) else {
+            return
+        }
+        
+        let request = URLRequest(url: loanUrl)
+        let task = URLSession.shared.dataTask(with: request, completionHandler: { (data, response, error) -> Void in
+            
+            if let error = error {
+                print(error)
+                return
+            }
+            
+            // Parse JSON data
+            if let data = data {
+                self.loans = self.parseJsonData(data: data)
+                
+                // Update table view's data
+                OperationQueue.main.addOperation({
+                    self.updateSnapshot()
+                })
+            }
+        })
+        
+        task.resume()
+    }
+
+    func parseJsonData(data: Data) -> [Loan] {
+        
+        var loans = [Loan]()
+        
+        do {
+            let jsonResult = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary
+            
+            // Parse JSON data
+            let jsonLoans = jsonResult?["loans"] as! [AnyObject]
+            for jsonLoan in jsonLoans {
+                var loan = Loan()
+                loan.name = jsonLoan["name"] as! String
+                loan.amount = jsonLoan["loan_amount"] as! Int
+                loan.use = jsonLoan["use"] as! String
+                let location = jsonLoan["location"] as! [String:AnyObject]
+                loan.country = location["country"] as! String
+                loans.append(loan)
+            }
+            
+        } catch {
+            print(error)
+        }
+        
+        return loans
+    }
+    
+    func configureDataSource() -> TableDDS {
+
+        let cellIdentifier = "Cell"
+
+        let dataSource = TableDDS(
+            tableView: tableView,
+            cellProvider: {  tableView, indexPath, loan in
+                let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! KivaLoanTableViewCell
+
+                cell.nameLabel.text = loan.name
+                cell.countryLabel.text = loan.country
+                cell.useLabel.text = loan.use
+                cell.amountLabel.text = "$\(loan.amount)"
+
+                return cell
+            }
+        )
+
+        return dataSource
+    }
+    
+    func updateSnapshot(animatingChange: Bool = false) {
+
+        // Create a snapshot and populate the data
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Loan>()
+        snapshot.appendSections([.all])
+        snapshot.appendItems(loans, toSection: .all)
+
+        dataSource.apply(snapshot, animatingDifferences: animatingChange)
+    }
 }
---
+++ Working with JSON and Codable/Working with JSON and Codable/TableDDS.swift
@@ -0,0 +1,14 @@
+//
+//  TableDDS.swift
+//  Working with JSON and Codable
+//
+//  Created by MRGS on 24.06.2022.
+//
+
+import UIKit
+enum Section {
+    case all
+}
+class TableDDS: UITableViewDiffableDataSource<Section, Loan> {
+
+}
