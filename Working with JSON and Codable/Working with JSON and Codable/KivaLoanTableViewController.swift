8aaf0efb39ea424c8a082b2962aa16b974039984
mrgsdev <mrgsdev@gmail.com>
2022-06-26 15:37:51 +0300

    FINAL

--- Working with JSON and Codable/Working with JSON and Codable/KivaLoanTableViewController.swift
+++ Working with JSON and Codable/Working with JSON and Codable/KivaLoanTableViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class KivaLoanTableViewController: UITableViewController {
-
+    
     private let kivaLoanURL = "https://api.kivaws.org/v1/loans/newest.json"
     private var loans = [Loan]() 
     
@@ -16,12 +16,12 @@ class KivaLoanTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         tableView.estimatedRowHeight = 92.0
         tableView.rowHeight = UITableView.automaticDimension
         getLatestLoans()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -55,61 +55,47 @@ extension KivaLoanTableViewController{
         
         task.resume()
     }
-
+    
     func parseJsonData(data: Data) -> [Loan] {
-        
         var loans = [Loan]()
-        
+        let decoder = JSONDecoder()
         do {
-            let jsonResult = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary
-            
-            // Parse JSON data
-            let jsonLoans = jsonResult?["loans"] as! [AnyObject]
-            for jsonLoan in jsonLoans {
-                var loan = Loan()
-                loan.name = jsonLoan["name"] as! String
-                loan.amount = jsonLoan["loan_amount"] as! Int
-                loan.use = jsonLoan["use"] as! String
-                let location = jsonLoan["location"] as! [String:AnyObject]
-                loan.country = location["country"] as! String
-                loans.append(loan)
-            }
-            
+            let loanDataStore = try decoder.decode(LoanDataStore.self, from: data)
+            loans = loanDataStore.loans
         } catch {
             print(error)
         }
-        
         return loans
     }
     
     func configureDataSource() -> TableDDS {
-
+        
         let cellIdentifier = "Cell"
-
+        
         let dataSource = TableDDS(
             tableView: tableView,
             cellProvider: {  tableView, indexPath, loan in
                 let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! KivaLoanTableViewCell
-
+                
                 cell.nameLabel.text = loan.name
                 cell.countryLabel.text = loan.country
                 cell.useLabel.text = loan.use
                 cell.amountLabel.text = "$\(loan.amount)"
-
+                
                 return cell
             }
         )
-
+        
         return dataSource
     }
     
     func updateSnapshot(animatingChange: Bool = false) {
-
+        
         // Create a snapshot and populate the data
         var snapshot = NSDiffableDataSourceSnapshot<Section, Loan>()
         snapshot.appendSections([.all])
         snapshot.appendItems(loans, toSection: .all)
-
+        
         dataSource.apply(snapshot, animatingDifferences: animatingChange)
     }
 }
