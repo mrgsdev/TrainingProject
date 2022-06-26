a2dcabbbfb8d57c32f79092f3c590ad35c02812f
mrgsdev <mrgsdev@gmail.com>
2022-06-26 22:41:02 +0300

    add new project SendingSMS&MMS(MessageUI)

---
+++ Sending SMS and MMS Using MessageUI/Sending SMS and MMS Using MessageUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sending SMS and MMS Using MessageUI
+//
+//  Created by MRGS on 26.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
