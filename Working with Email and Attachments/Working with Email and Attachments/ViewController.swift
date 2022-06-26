65049cd69661709bf69969159fbf6d827b09d942
mrgsdev <mrgsdev@gmail.com>
2022-06-26 15:40:05 +0300

    add new project Working with Email and Attachments

---
+++ Working with Email and Attachments/Working with Email and Attachments/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Working with Email and Attachments
+//
+//  Created by MRGS on 26.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
