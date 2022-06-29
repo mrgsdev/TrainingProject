003687478aa7db0ef7e75f8bdb344e78c4831c1f
mrgsdev <mrgsdev@gmail.com>
2022-06-29 16:04:03 +0300

    add Search for Nearby Points of Interest Using Local Search

---
+++ Search for Nearby Points of Interest Using Local Search/Search for Nearby Points of Interest Using Local Search/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Search for Nearby Points of Interest Using Local Search
+//
+//  Created by MRGS on 29.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
