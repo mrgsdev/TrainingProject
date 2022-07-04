8c4c7a0acf2dcf89b75d04d46e38af0d0755b3b8
mrgsdev <mrgsdev@gmail.com>
2022-07-04 10:39:10 +0300

    add Video Capturing and Playback Using AVKit

---
+++ Video Capturing and Playback (AVKit)/Video Capturing and Playback (AVKit)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Video Capturing and Playback (AVKit)
+//
+//  Created by MRGS on 04.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
