72b2fe48588e1696a67232e55b88a4431eca3523
mrgsdev <mrgsdev@gmail.com>
2022-07-04 10:53:23 +0300

    Saving Video Data to a Movie File

--- Video Capturing and Playback (AVKit)/Video Capturing and Playback (AVKit)/SimpleVideoCamController.swift
+++ Video Capturing and Playback (AVKit)/Video Capturing and Playback (AVKit)/SimpleVideoCamController.swift
@@ -12,11 +12,14 @@ class SimpleVideoCamController: UIViewController {
     @IBOutlet var cameraButton:UIButton!
     
     var currentDevice: AVCaptureDevice!
+    var videoFileOutput: AVCaptureMovieFileOutput!
     let captureSession = AVCaptureSession()
+    var cameraPreviewLayer: AVCaptureVideoPreviewLayer?
+    var isRecording = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        configure()
         // Do any additional setup after loading the view.
     }
     
@@ -39,6 +42,19 @@ class SimpleVideoCamController: UIViewController {
         guard let captureDeviceInput = try? AVCaptureDeviceInput(device: currentDevice) else {
             return
         }
+        // Configure the session with the output for capturing video
+        videoFileOutput = AVCaptureMovieFileOutput()
+        // Configure the session with the input and the output devices
+        captureSession.addInput(captureDeviceInput)
+        captureSession.addOutput(videoFileOutput)
+        // Provide a camera preview
+        cameraPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        view.layer.addSublayer(cameraPreviewLayer!)
+        cameraPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        cameraPreviewLayer?.frame = view.layer.frame
+        // Bring the camera button to front
+        view.bringSubviewToFront(cameraButton)
+        captureSession.startRunning()
     }
     // MARK: - Action methods
     
@@ -47,6 +63,29 @@ class SimpleVideoCamController: UIViewController {
     }
     
     @IBAction func capture(sender: AnyObject) {
+        if !isRecording {
+                isRecording = true
+            UIView.animate(withDuration: 0.5, delay: 0.0, options: [.repeat, .autoreverse, .allowUserInteraction], animations: { () -> Void in
+            self.cameraButton.transform = CGAffineTransform(scaleX: 0.5, y: 0.5) }, completion: nil)
+            let outputPath = NSTemporaryDirectory() + "output.mov"
+            let outputFileURL = URL(fileURLWithPath: outputPath)
+            videoFileOutput?.startRecording(to: outputFileURL, recordingDelegate: self)
+        }else{
+            isRecording = true
+            UIView.animate(withDuration: 0.5, delay: 1.0, options: [], animations: { ( ) -> Void in
+                self.cameraButton.transform = CGAffineTransform(scaleX: 1.0, y: 1.0) }, completion: nil)
+            cameraButton.layer.removeAllAnimations()
+            videoFileOutput?.stopRecording()
+        }
+
     }
     
+}
+extension SimpleVideoCamController:AVCaptureFileOutputRecordingDelegate {
+    func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
+        guard error == nil else {
+                    print(error ?? "")
+        return
+        }
+    }
 }
