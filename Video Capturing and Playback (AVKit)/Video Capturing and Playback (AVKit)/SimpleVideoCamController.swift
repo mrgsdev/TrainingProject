725ecfca690dc40538473cc589c7c29dc14df338
mrgsdev <mrgsdev@gmail.com>
2022-07-04 10:45:41 +0300

    Selecting the Input Device

--- Video Capturing and Playback (AVKit)/Video Capturing and Playback (AVKit)/SimpleVideoCamController.swift
+++ Video Capturing and Playback (AVKit)/Video Capturing and Playback (AVKit)/SimpleVideoCamController.swift
@@ -6,23 +6,40 @@
 //
 
 import UIKit
- 
+import AVFoundation
 class SimpleVideoCamController: UIViewController {
-
+    
     @IBOutlet var cameraButton:UIButton!
+    
+    var currentDevice: AVCaptureDevice!
+    let captureSession = AVCaptureSession()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    private func configure() {
+        // Preset the session for taking photo in full resolution
+        captureSession.sessionPreset = AVCaptureSession.Preset.high
+        
+        // Get the back-facing camera for capturing videos
+        guard let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) else {
+            print("Failed to get the camera device")
+            return
+        }
+        currentDevice = device
+        // Get the input data source
+        guard let captureDeviceInput = try? AVCaptureDeviceInput(device: currentDevice) else {
+            return
+        }
+    }
     // MARK: - Action methods
     
     @IBAction func unwindToCamera(segue:UIStoryboardSegue) {
@@ -31,5 +48,5 @@ class SimpleVideoCamController: UIViewController {
     
     @IBAction func capture(sender: AnyObject) {
     }
-
+    
 }
