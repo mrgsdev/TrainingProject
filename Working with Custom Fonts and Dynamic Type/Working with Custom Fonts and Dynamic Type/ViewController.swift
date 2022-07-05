7e46b1f8af70f78ee2e70ab2fbc8a8816073feb2
mrgsdev <mrgsdev@gmail.com>
2022-07-05 09:43:27 +0300

    Final(CustomFont+DynamicType)

--- Working with Custom Fonts and Dynamic Type/Working with Custom Fonts and Dynamic Type/ViewController.swift
+++ Working with Custom Fonts and Dynamic Type/Working with Custom Fonts and Dynamic Type/ViewController.swift
@@ -19,9 +19,23 @@ class ViewController: UIViewController {
 //        label2.font = UIFont(name: "Hallo sans", size: 30.0)
 //        label3.font = UIFont(name: "Canter Light", size: 35.0)
 ///     Dynamic Type
-        label1.font = UIFont.preferredFont(forTextStyle: .title1)
-        label2.font = UIFont.preferredFont(forTextStyle: .headline)
-        label3.font = UIFont.preferredFont(forTextStyle: .subheadline)
+//        label1.font = UIFont.preferredFont(forTextStyle: .title1)
+//        label2.font = UIFont.preferredFont(forTextStyle: .headline)
+//        label3.font = UIFont.preferredFont(forTextStyle: .subheadline)
+//
+/// CustomFont + DynamicType
+
+        if let customFont1 = UIFont(name: "Mohave-Italic", size: 28.0) { let fontMetrics = UIFontMetrics(forTextStyle: .title1)
+            label1.font = fontMetrics.scaledFont(for: customFont1)
+        }
+        if let customFont2 = UIFont(name: "Hallo sans", size: 20.0) {
+            let fontMetrics = UIFontMetrics(forTextStyle: .headline)
+            label2.font = fontMetrics.scaledFont(for: customFont2)
+        }
+        if let customFont3 = UIFont(name: "CanterLight", size: 17.0) {
+            let fontMetrics = UIFontMetrics(forTextStyle: .subheadline)
+            label3.font = fontMetrics.scaledFont(for: customFont3)
+        }
         
         label1.adjustsFontForContentSizeCategory = true
         label2.adjustsFontForContentSizeCategory = true
