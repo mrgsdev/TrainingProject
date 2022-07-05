b871769e62b246310289eb42647557f3c9c29ff8
mrgsdev <mrgsdev@gmail.com>
2022-07-05 09:17:18 +0300

    add Working with Custom Fonts and Dynamic Type

---
+++ Working with Custom Fonts and Dynamic Type/Working with Custom Fonts and Dynamic Type/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Working with Custom Fonts and Dynamic Type
+//
+//  Created by MRGS on 05.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
