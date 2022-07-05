e205ee6b7fc186513b1003fd3f801b0fe1ce7220
mrgsdev <mrgsdev@gmail.com>
2022-07-05 09:26:36 +0300

    add Fonts & UI

--- Working with Custom Fonts and Dynamic Type/Working with Custom Fonts and Dynamic Type/ViewController.swift
+++ Working with Custom Fonts and Dynamic Type/Working with Custom Fonts and Dynamic Type/ViewController.swift
@@ -8,11 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+     
 
 
 }
