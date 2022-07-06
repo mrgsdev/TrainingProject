a238bde69ea32025237fd4e66c8a29a6f5272e37
mrgsdev <mrgsdev@gmail.com>
2022-07-06 09:56:22 +0300

    add Working with AirDrop

---
+++ Working with AirDrop, UIActivityViewController and Uniform Type Identifiers/Working with AirDrop, UIActivityViewController and Uniform Type Identifiers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Working with AirDrop, UIActivityViewController and Uniform Type Identifiers
+//
+//  Created by MRGS on 06.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
