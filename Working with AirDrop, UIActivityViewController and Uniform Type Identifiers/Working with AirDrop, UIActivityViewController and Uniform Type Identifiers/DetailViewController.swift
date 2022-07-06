3cff006cd4158b4e26e940f987f41784a04bfe13
mrgsdev <mrgsdev@gmail.com>
2022-07-06 10:08:56 +0300

    Adding a Share Button in Interface Builder

--- Working with AirDrop, UIActivityViewController and Uniform Type Identifiers/Working with AirDrop, UIActivityViewController and Uniform Type Identifiers/DetailViewController.swift
+++ Working with AirDrop, UIActivityViewController and Uniform Type Identifiers/Working with AirDrop, UIActivityViewController and Uniform Type Identifiers/DetailViewController.swift
@@ -8,21 +8,21 @@
 import UIKit
 import WebKit
 class DetailViewController: UIViewController {
-
+    
     @IBOutlet var webView: WKWebView!
     
     var filename = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Get the full path of the file
         if let fileURL = fileToURL(file: filename) {
             let urlRequest = URLRequest(url: fileURL)
             webView.load(urlRequest)
         }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -39,6 +39,25 @@ class DetailViewController: UIViewController {
         return nil
     }
     @IBAction func share(sender: AnyObject) {
-        
+        if let fileURL = fileToURL(file: filename) {
+            let objectsToShare = [fileURL]
+            let activityController = UIActivityViewController(activityItems: objectsToShare, applicationActivities: nil)
+            let activityTypeType = UIActivity.ActivityType.self
+            let excludedActivities = [
+                activityTypeType.postToFlickr,
+                activityTypeType.postToWeibo,
+                activityTypeType.message,
+                activityTypeType.mail,
+                activityTypeType.print,
+                activityTypeType.copyToPasteboard,
+                activityTypeType.assignToContact,
+                activityTypeType.saveToCameraRoll,
+                activityTypeType.addToReadingList,
+                activityTypeType.postToFlickr,
+                activityTypeType.postToVimeo,
+                activityTypeType.postToTencentWeibo
+            ]
+            activityController.excludedActivityTypes = excludedActivities
+            present(activityController, animated: true, completion: nil) }
     }
 }
