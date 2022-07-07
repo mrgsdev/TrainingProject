927f9ab67e3dceef006ed004624857ffb05be6b5
mrgsdev <mrgsdev@gmail.com>
2022-07-07 13:29:30 +0300

    add DetailViewController

---
+++ Grid Layouts Using Collection Views and Diffable Data Source/Grid Layouts Using Collection Views and Diffable Data Source/DetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailViewController.swift
+//  Grid Layouts Using Collection Views and Diffable Data Source
+//
+//  Created by MRGS on 07.07.2022.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
