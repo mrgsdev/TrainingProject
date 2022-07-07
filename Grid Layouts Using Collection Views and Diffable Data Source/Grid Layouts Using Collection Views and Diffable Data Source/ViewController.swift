e468767d7f6e0c4894ca5ba88330c8a7e4c12aa3
mrgsdev <mrgsdev@gmail.com>
2022-07-07 12:16:23 +0300

    add Grid Layouts Using Collection Views and Diffable Data Source

---
+++ Grid Layouts Using Collection Views and Diffable Data Source/Grid Layouts Using Collection Views and Diffable Data Source/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Grid Layouts Using Collection Views and Diffable Data Source
+//
+//  Created by MRGS on 07.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
