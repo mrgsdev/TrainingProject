2a7d0562b8ff6ea62099c2ee52d64a10e21486f6
mrgsdev <mrgsdev@gmail.com>
2022-07-07 12:32:38 +0300

    add UI

---
+++ Grid Layouts Using Collection Views and Diffable Data Source/Grid Layouts Using Collection Views and Diffable Data Source/IconCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  IconCollectionViewCell.swift
+//  Grid Layouts Using Collection Views and Diffable Data Source
+//
+//  Created by MRGS on 07.07.2022.
+//
+
+import UIKit
+
+class IconCollectionViewCell: UICollectionViewCell {
+
+}
