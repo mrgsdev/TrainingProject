3e368f40f512d09bb1b1d9a84acf7c4162005a28
mrgsdev <mrgsdev@gmail.com>
2022-07-08 11:16:15 +0300

    add Collection Views Using UITraitCollection and Compositional Layout

---
+++ Collection Views Using UITraitCollection and Compositional Layout/Collection Views Using UITraitCollection and Compositional Layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Collection Views Using UITraitCollection and Compositional Layout
+//
+//  Created by MRGS on 08.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
