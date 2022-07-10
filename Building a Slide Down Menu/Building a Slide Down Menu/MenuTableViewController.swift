8354d7836bdc4fa5660fa6c02268dcb6b1331359
mrgsdev <mrgsdev@gmail.com>
2022-07-10 14:57:17 +0300

    add UI

---
+++ Building a Slide Down Menu/Building a Slide Down Menu/MenuTableViewController.swift
@@ -0,0 +1,46 @@
+//
+//  MenuTableViewController.swift
+//  Building a Slide Down Menu
+//
+//  Created by MRGS on 10.07.2022.
+//
+
+import UIKit
+
+class MenuTableViewController: UITableViewController {
+    
+    var menuItems = ["Home", "News", "Tech", "Finance", "Reviews"]
+    var currentItem = "Home"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // Return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Return the number of rows
+        return menuItems.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! MenuTableViewCell
+
+        // Configure the cell...
+        cell.titleLabel.text = menuItems[indexPath.row]
+        cell.titleLabel.textColor = (menuItems[indexPath.row] == currentItem) ? UIColor.white : UIColor.gray
+
+        return cell
+    }
+}
