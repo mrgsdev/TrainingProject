51eb0fa8ec8882121585c62608a0ea9e370eadd5
mrgsdev <mrgsdev@gmail.com>
2022-07-12 11:08:09 +0300

    adding Building a Weather Widget Using WidgetKit

---
+++ Building a Weather Widget Using WidgetKit/Building a Weather Widget Using WidgetKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Building a Weather Widget Using WidgetKit
+//
+//  Created by MRGS on 12.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
