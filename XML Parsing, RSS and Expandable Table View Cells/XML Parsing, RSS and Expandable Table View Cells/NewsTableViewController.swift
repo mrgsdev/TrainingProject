0ad221057b6a994614ac5826bfeab17182ca7da8
mrgsdev <mrgsdev@gmail.com>
2022-07-13 13:07:45 +0300

    FINAL

--- XML Parsing, RSS and Expandable Table View Cells/XML Parsing, RSS and Expandable Table View Cells/NewsTableViewController.swift
+++ XML Parsing, RSS and Expandable Table View Cells/XML Parsing, RSS and Expandable Table View Cells/NewsTableViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class NewsTableViewController: UITableViewController {
+    
+    enum CellState {
+        case expanded
+        case collapsed
+    }
+    
+    private var rssItems: [ArticleItem]?
+    
+    private var cellStates: [CellState]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.estimatedRowHeight = 155.0
         tableView.rowHeight = UITableView.automaticDimension
 
+        let feedParser = FeedParser()
+        feedParser.parseFeed(feedUrl: "https://www.nasa.gov/rss/dyn/breaking_news.rss", completionHandler: {
+            (rssItems: [ArticleItem]) -> Void in
+            
+            self.rssItems = rssItems
+            self.cellStates = [CellState](repeating: .collapsed, count: rssItems.count)
+            
+            OperationQueue.main.addOperation({ () -> Void in
+                self.tableView.reloadSections(IndexSet(integer: 0), with: .none)
+            })
+        })
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,5 +48,43 @@ class NewsTableViewController: UITableViewController {
     override func numberOfSections(in tableView: UITableView) -> Int {
         // Return the number of sections
         return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Return the number of rows in the section.
+        guard let rssItems = rssItems else {
+            return 0
+        }
+        
+        return rssItems.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! NewsTableViewCell
+        
+        // Configure the cell...
+        if let item = rssItems?[indexPath.row] {
+            
+            cell.titleLabel.text = item.title
+            cell.descriptionLabel.text = item.description
+            cell.dateLabel.text = item.pubDate
+            
+            if let cellStates = cellStates {
+                cell.descriptionLabel.numberOfLines = (cellStates[indexPath.row] == .expanded) ? 0 : 4
+            }
+        }
+        
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let cell = tableView.cellForRow(at: indexPath) as! NewsTableViewCell
+        
+        tableView.beginUpdates()
+        cell.descriptionLabel.numberOfLines = (cell.descriptionLabel.numberOfLines == 0) ? 4 : 0
+        cellStates?[indexPath.row] = (cell.descriptionLabel.numberOfLines == 0) ? .expanded : .collapsed
+        tableView.endUpdates()
     }
 }
