72d1525246d6d6c042d8d45ce1455b41a593c780
mrgsdev <mrgsdev@gmail.com>
2022-07-17 15:16:29 +0300

    add Applying a Blurred Background Using UIVisualEffect

---
+++ Applying a Blurred Background Using UIVisualEffect/Applying a Blurred Background Using UIVisualEffect/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Applying a Blurred Background Using UIVisualEffect
+//
+//  Created by MRGS on 17.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
