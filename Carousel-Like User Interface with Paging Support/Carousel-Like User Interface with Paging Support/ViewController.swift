fa5ad47e64c7e77b4c73a406d8ee025b7e669792
mrgsdev <mrgsdev@gmail.com>
2022-07-19 10:56:31 +0300

    add Carousel-Like User Interface with Paging Support

---
+++ Carousel-Like User Interface with Paging Support/Carousel-Like User Interface with Paging Support/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Carousel-Like User Interface with Paging Support
+//
+//  Created by MRGS on 19.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
