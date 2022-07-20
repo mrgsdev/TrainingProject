29e84ae5d5f7cf221b9ef3a4611e10277f3991ca
mrgsdev <mrgsdev@gmail.com>
2022-07-20 13:03:22 +0300

    add Working with Parse as Mobile Backends

---
+++ Working with Parse as Mobile Backends/Working with Parse as Mobile Backends/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Working with Parse as Mobile Backends
+//
+//  Created by MRGS on 20.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
